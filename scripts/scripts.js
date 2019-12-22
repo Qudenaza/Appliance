099901291a3b745c3f9baeca431a479241b56ded
qudenaza <qudenaza@gmail.com>
2019-12-22 15:04:14 +0300

    Steps section

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -205,3 +205,13 @@
     })
   }
 }
+
+{
+  $(function () {
+    $("a[href^='#']").click(function () {
+      let _href = $(this).attr("href");
+      $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+      return false;
+    });
+  });
+}
